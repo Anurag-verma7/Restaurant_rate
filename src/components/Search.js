985ab4e9b6e3c7bea4735b433f8791f46ab52067
Anurag-verma7 <anuragverma2651@gmail.com>
2020-06-26 23:33:34 +0530

    changes for better routing

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Restaurant from './restaurant'
+import { BrowserRouter } from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form,FormControl,Button,Row,Col,InputGroup, Container } from 'react-bootstrap';
 import './Search.css'
+import {Link} from 'react-router-dom'
 const Search1 = () =>{
   return(
     <Container className="center">
@@ -10,7 +14,8 @@ const Search1 = () =>{
       placeholder="search location"
     />
     <InputGroup.Append>
-      <Button variant="outline-orange" className="search">Search</Button>
+      <Button variant="outline-orange" className="search"><Link to ="/restaurant">search</Link></Button>
+     
     </InputGroup.Append>
   </InputGroup>
   </Container>
