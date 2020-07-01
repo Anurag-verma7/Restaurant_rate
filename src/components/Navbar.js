52f37e958c7c2c522ed0450d33bf955a484b08cb
Anurag-verma7 <anuragverma2651@gmail.com>
2020-07-01 21:53:11 +0530

    modal incorporated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{useState ,useEffect} from 'react';
 import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form,FormControl,Button,Nav,NavDropdown,Navbar } from 'react-bootstrap';
+import { Form,FormControl,Button,Nav,NavDropdown,Navbar,Modal } from 'react-bootstrap';
 const Navbar1 = () =>{
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return(
     <div className="shadow-box-example hoverable z-depth-5" style={{height: "50px", width: "100%", backgroundColor: "#64b5f6"}}>
     <Navbar collapseOnSelect expand="lg" bg="dark" fixed="top" variant="dark" >
@@ -23,11 +27,29 @@ const Navbar1 = () =>{
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ml-auto">
       <Nav.Link href="#">Login</Nav.Link>
-      <Nav.Link href="#">Signup</Nav.Link>
+      <Nav.Link href="#" onClick={handleShow}>Signup</Nav.Link>
       
     </Nav>
   </Navbar.Collapse>
 </Navbar>
+<Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>signup</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>yaha signup kare!!!!!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            signup
+          </Button>
+        </Modal.Footer>
+      </Modal>
 </div>
   )
 }
